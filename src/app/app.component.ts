ab81f5db8a256614277cfe1edd224fd59958a053
azran4u <azran4u@gmail.com>
2018-09-13 09:16:48 +0300

    basic features working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Cat, CatService} from './cat.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoArray = [];
+  title = 'My first Todo App';
+  public cats: Cat[];
+
+  constructor( private catService: CatService ){};
+
+  addTodo(value) {
+    this.todoArray.push(value);
+    console.log("item added:" + value);
+
+    this.catService.getAllCats()
+      .subscribe(
+        res => {
+          let cats : Cat[] = res["cats"];
+          cats.forEach( (cat) => {
+            console.log("Received from cat service: " + cat.name);
+          })},
+        err => {
+          console.log("Error: " + err.name + ". " + err.message );
+        }
+      );
+  }
+
+  /*delete item*/
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo == this.todoArray[i]) {
+        this.todoArray.splice(i, 1)
+      }
+    }
+    console.log("item deleted: " + todo.toString())
+  }
+
+  // submit Form
+  todoSubmit(value:any){
+    if(value!==""){
+      this.addTodo(value.todo)
+    }else{
+      alert('Field required **')
+    }
+  }
 }
